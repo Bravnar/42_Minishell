f040304cf289dd8b5b207b9f262d09ebadf491cf
Bravnar <stan.muravyev@bluewin.ch>
2024-05-04 19:01:26 +0200

    quick additions

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -1,43 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bravnar <bravnar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/04 12:59:58 by bravnar           #+#    #+#             */
-/*   Updated: 2024/05/04 18:48:48 by bravnar          ###   ########.fr       */
+/*   Created: 2024/05/04 18:55:15 by bravnar           #+#    #+#             */
+/*   Updated: 2024/05/04 18:57:49 by bravnar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	gameplay_loop(t_main *shell)
+void	parser(t_main *shell)
 {
+	t_tok	*tok;
 	t_lex	*lex;
-	
-	lex = shell->l;
-	while (true)
-	{
-		lex->input = readline(shell->prompt);
-		if (!lex->input)
-			break ;
-		if (lex->input[0])
-			add_history(lex->input);
-		lexer(lex);
-		parser(shell);
-		free(lex->input);
-		free_tokens(lex->link);
-		lex->link = NULL;
-	}
-}
 
-int	main(int ac, char **av, char **envp)
-{
-	t_main	*shell;
-	
-	(void)	ac;
-	(void)	av;
-	shell = init_structs(envp);
-	gameplay_loop(shell);
+	tok	= shell->tok;
+	lex = shell->l;
+	assign_types(tok, lex);
+	assign_index(tok, lex);
 }
