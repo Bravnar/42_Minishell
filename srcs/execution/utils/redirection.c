8a83411f7035a71b5afa705f8a8176a66c282429
Hadrien Louis Michel Morand <hmorand@c3r4s2.42lausanne.ch>
2024-06-14 11:29:53 +0200

    Execution fixes

--- srcs/execution/utils/redirection.c
+++ srcs/execution/utils/redirection.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-int	redirect_output(t_files *outfile, t_main *shell)
+void	redirect_output(t_files *outfile, t_main *shell)
 {
 	int	fd;
 
@@ -8,12 +8,14 @@ int	redirect_output(t_files *outfile, t_main *shell)
 		fd = open(outfile->file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	else
 		fd = open(outfile->file_name, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (fd == -1)
+		perror("Failed to open output file");
 	if (dup2(fd, shell->out) == -1)
 	{
 		ft_fprintf(shell->err, "dup2\n");
 		exit(EXIT_FAILURE);
 	}
-	return (fd);
+	close(fd);
 }
 
 int	redirect_input(t_files *infile)
@@ -21,5 +23,7 @@ int	redirect_input(t_files *infile)
 	int	fd;
 
 	fd = open(infile->file_name, O_RDONLY);
+	if (fd == -1)
+		perror("Failed to open input file");
 	return (fd);
 }
