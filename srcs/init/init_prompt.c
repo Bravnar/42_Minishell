385d39ecad289ff93ed224db7af78ebdb3055a55
Stanislav Muravyev <smuravye@c3r3s1.42lausanne.ch>
2024-06-18 10:40:39 +0200

    fixed prompt generation leak

--- srcs/init/init_prompt.c
+++ srcs/init/init_prompt.c
@@ -5,6 +5,8 @@ char	*create_home(char **part)
 	char	*result;
 	char	*tmp;
 
+	if (!part)
+		return (NULL);
 	result = ft_strdup("/");
 	tmp = NULL;
 	if (part[0] && part[1])
@@ -33,16 +35,10 @@ char	*join_prompt(char **part, t_main *shell)
 	char	*tmp;
 	char	*home;
 
-	// if (!part)
-	// 	return (NULL);
-	// if (!part[0])
-	// 	return ft_strdup("/");
-	if (!part || !part[0])
+	home = create_home(part);
+	if (!part || !part[0] || !home)
 		return (ft_strdup("/"));
 	i = 0;
-	home = create_home(part);
-	if (!home)
-		return (ft_strdup("/"));
 	if (!ft_strcmp(home, shell->home))
 	{
 		result = ft_strdup("~");
@@ -55,39 +51,28 @@ char	*join_prompt(char **part, t_main *shell)
 	{
 		tmp = ft_better_join(result, "/");
 		free(result);
-		result = ft_better_join(tmp, part[i]);
+		result = ft_better_join(tmp, part[i++]);
 		free (tmp);
-		i++;
 	}
 	return (result);
 }
 
-
-/* Refactor to reduce (26 lines!!)
-   Function that creates the prompt 
-   It uses a group of defined strings to
-   strjoin them and output to the prompt */
-void	terminal_prompt(t_main *shell)
+void	terminal_prompt_nonroot(t_main *shell, char **path)
 {
-	char	**cwd;
+	char	**breakdown;
 	char	*tmp;
 	char	*full_start;
 	char	*end;
 	char	*final;
-	char	*path;
 
-	if (!shell->has_env)
-		return ;
-	if (shell->prompt)
-		free(shell->prompt);
-	path = my_get_path();
-	cwd = ft_split(path, '/');
-	free(path);
+	breakdown = NULL;
+	breakdown = ft_split(*path, '/');
+	free(*path);
 	tmp = ft_better_join(G_ARROW_SIGN, MINISH);
 	full_start = ft_better_join(tmp, EHL);
 	free(tmp);
-	tmp = join_prompt(cwd, shell);
-	ft_free_arr(cwd);
+	tmp = join_prompt(breakdown, shell);
+	ft_free_arr(breakdown);
 	end = ft_better_join(tmp, X_SIGN);
 	final = ft_better_join(full_start, end);
 	shell->prompt = ft_strdup(final);
@@ -97,73 +82,41 @@ void	terminal_prompt(t_main *shell)
 	free(final);
 }
 
-/* char	*set_cwd_start(t_main *shell)
+void	terminal_prompt_root(t_main *shell, char **path)
 {
-	char	*path_2;
-	char	*path_3;
-	char	*path_4;
-	char	*result;
-
-	if (shell->tmp_path)
-		free(shell->tmp_path);
-	shell->tmp_path = my_get_path();
-	path_2 = ft_strchr(path + 1, '/');
-	path_3 = ft_strchr(path_2 + 1, '/');
-	path_4 = ft_strndup(path, path_2 - path);
-	if (!ft_strcmp(shell->tmp_path, "/") || !ft_strcmp(shell->tmp_path, path_4))
-		result = ft_strdup(shell->tmp_path);
-	else
-		result = ft_strdup("~");
-	free(path);
-	free(path_4);
-	return (result);
-}
-char	*cwd_join(char *start, char *cwd)
-{
-	int	len;
-
-	len = ft_strlen(shell->home);
-
-}
-
-char	*set_cwd(t_main *shell)
-{
-	char	*start_cwd;
+	char	*tmp;
+	char	*full_start;
+	char	*end;
 	char	*final;
-	//char	*cwd;
 
-	start_cwd = NULL;
-	start_cwd = set_cwd_start(shell);
-	//cwd = my_get_path();
-	if (!ft_strcmp(shell->tmp_path, shell->home))
-		final = ft_strdup(start_cwd);
-	else
-		final = cwd_join(start_cwd, shell->tmp_path);
-	free(start_cwd);
-	//free(cwd);
-	return (final);
+	free(*path);
+	tmp = ft_better_join(G_ARROW_SIGN, MINISH);
+	full_start = ft_better_join(tmp, EHL);
+	free(tmp);
+	tmp = join_prompt(NULL, shell);
+	end = ft_better_join(tmp, X_SIGN);
+	final = ft_better_join(full_start, end);
+	shell->prompt = ft_strdup(final);
+	free(tmp);
+	free(full_start);
+	free(end);
+	free(final);
 }
 
 void	terminal_prompt(t_main *shell)
 {
-	char	*cwd;
-	char	*part_1;
-	char	*part_2;
-	char	*part_3;
-	char	*final;
+	char	*path;
 
-	cwd = set_cwd(shell);
-	part_1 = ft_better_join(G_ARROW_SIGN, MINISH);
-	part_2 = ft_better_join(part_1, EHL);
-	part_3 = ft_better_join(part_2, cwd);
-	final = ft_better_join(part_3, X_SIGN);
-	shell->prompt = ft_strdup(final);
-	free(cwd);
-	free(part_1);
-	free(part_2);
-	free(part_3);
-	free(final);
-} */
+	if (!shell->has_env)
+		return ;
+	if (shell->prompt)
+		free(shell->prompt);
+	path = my_get_path();
+	if (ft_strcmp(path, "/"))
+		terminal_prompt_nonroot(shell, &path);
+	else
+		terminal_prompt_root(shell, &path);
+}
 
 /* Additional function to debug - not needed otherwise */
 void print_prompt_info(const char *prompt) 
